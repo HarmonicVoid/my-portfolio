4e22fdc64c6b04c7065d4a061300e01bc9a5bfb6
HarmonicVoid <marvin.bolanos25@gmail.com>
2020-12-14 06:06:10 +0000

    title change

--- components/HomeTab.tsx
+++ components/HomeTab.tsx
@@ -18,7 +18,7 @@ function HomeTab(){
             
             <Box margin={{top: '3vh', bottom: '3vh', left: '3vh', right: '3vh'}} animation="fadeIn" pad="small" background="#e6da00">
               <Paragraph textAlign="center" size="xxlarge" color="#2d3748">
-              <strong>Hello, my name is Marvin and I am a Freelance Software Developer</strong>
+              <strong>Hello, my name is Marvin and I am a freelance Software Developer</strong>
               </Paragraph>
             </Box>
           
