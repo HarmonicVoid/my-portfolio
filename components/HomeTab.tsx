d9d06716ebcf583f2972278d74316236d91430e9
HarmonicVoid <marvin.bolanos25@gmail.com>
2020-12-19 19:10:27 +0000

    material ui:

--- components/HomeTab.tsx
+++ components/HomeTab.tsx
@@ -21,21 +21,21 @@ function HomeTab(){
                 }
               },
             }}>
-          <Box margin={{top: '5vh'}} align="center">
-            
-            <Box align="center"
-                  justify="center"
-                  direction="row"
-                  gap="small"
-                  pad="small">
-                  <Avatar src="/avatar.png" size="200px" />
-                </Box>
-                <Box margin={{top: '3vh', bottom: '3vh', left: '3vh', right: '3vh'}} animation="zoomOut" pad="small" background="#e6da00">
-                  <Paragraph textAlign="center" size="xxlarge" color="#2d3748">
-                  <strong>Hello, my name is Marvin and I am a self-taught Software Developer</strong>
-                  </Paragraph>
-                </Box>
-          </Box>
+            <Box margin={{top: '5vh', left: '5vh', right: '5vh'}} align="center">
+              
+              <Box align="center"
+                    justify="center"
+                    direction="row"
+                    gap="small"
+                    pad="small">
+                    <Avatar src="/avatar.png" size="200px" />
+                  </Box>
+                  <Box margin={{top: '3vh', bottom: '3vh', left: '3vh', right: '3vh'}} pad="small" background="#e6da00">
+                    <Paragraph textAlign="center" size="xxlarge" color="#2d3748">
+                    <strong>Hello, my name is Marvin and I am a self-taught Software Developer</strong>
+                    </Paragraph>
+                  </Box>
+            </Box>
           </motion.div>    
 
       </>
